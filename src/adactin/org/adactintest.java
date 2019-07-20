24a437a50dc460441f7438ff9bb43a17092d25bd
mohan3646 <mohanvel.1988@gmail.com>
2019-07-20 13:25:40 +0530

    added

--- src/adactin/org/adactintest.java
+++ src/adactin/org/adactintest.java
@@ -9,44 +9,40 @@ import org.openqa.selenium.support.ui.Select;
 public class adactintest {
 
 	public static void main(String[] args) {
-		
-		System.setProperty("webdriver.chrome.driver","D:\\mohan\\adactin\\driver\\chromedriver.exe");
-		
+
+		System.setProperty("webdriver.chrome.driver", "D:\\mohan\\adactin\\driver\\chromedriver.exe");
+
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.get("https://www.adactin.com/HotelApp/");
-		
+
 		driver.findElement(By.id("username")).sendKeys("Mohan1234");
-		
+
 		driver.findElement(By.id("password")).sendKeys("Mohan@1234");
-		
+
 		driver.findElement(By.id("login")).click();
-		
+
 		WebElement loc = driver.findElement(By.id("location"));
-		
+
 		Select s = new Select(loc);
 		s.selectByValue("London");
-		
+
 		WebElement h = driver.findElement(By.id("hotels"));
-		
+
 		Select ho = new Select(h);
-		
+
 		ho.selectByValue("Hotel creek");
-		
-	WebElement r = driver.findElement(By.id("room_type"));
-	
-	Select ro = new Select(r);
-	
-	ro.selectByValue("Standard");
-	
-	
-	
-	
-	
-		
-	
-		}
+
+		WebElement r = driver.findElement(By.id("room_type"));
+
+		Select ro = new Select(r);
+
+		ro.selectByValue("Standard");
+
+		int a = 10;
+		int b = 20;
+		System.out.println(a + b);
 
 	}
 
-
+}
